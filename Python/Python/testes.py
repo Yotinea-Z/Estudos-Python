0036a3ff1892833161a8889e12950ac98cf67630
Yotinea-Z <thalita.barros205@gmail.com>
2026-06-07 15:03:28 -0300

    Add files via upload

---
+++ Python/Python/testes.py
@@ -0,0 +1,8 @@
+print(5 + 5)
+input('')
+nome = input('Seu nome é?: ')
+print(f'nome de usuario:{nome}')
+a = 15
+print('o tipo de a é', type(a))
+b = input('Digite algo ')
+print('Só há espaços?', b.isspace())
