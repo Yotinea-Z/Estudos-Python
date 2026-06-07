02f820be61159db2de72cdb32949e4698c05370d
Yotinea-Z <thalita.barros205@gmail.com>
2026-06-07 12:23:50 -0300

    Delete Estudos directory

--- Estudos/Python/primeiraAula.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Digite um número inteiro "))
-num2 = int(input("Digite outro número inteiro "))
-soma = num1 + num2
-sub = num1 - num2
-print(f"A soma dos 2 números é {soma}")
-print(f"A subtração dos 2 números é {sub}")
-
